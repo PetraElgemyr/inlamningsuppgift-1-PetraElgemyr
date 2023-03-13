f7818627c8944e51d929e1d28c1ea1c0e4471ccb
petraelgemyr <petra.elgemyr@gmail.com>
2023-03-13 15:23:17 +0100

    js-files fixed, file path/structure fixed

--- src/js/toggleDarkmode.js
+++ src/js/toggleDarkmode.js
@@ -1,19 +1,13 @@
-const toggleLightModeButton = document.querySelector('.toggle-btn');
-toggleLightModeButton.addEventListener('click', toggleLightMode);
-console.log(toggleLightModeButton);
-
-export function toggleLightMode (){
-    document.body.classList.toggle('darkmode')
-    if (document.body.classList.contains('darkmode')){
-        toggleLightModeButton.innerHTML='Välj mörkt läge'
-        console.log('mörkt läge');
-    } 
-    
-    else {
-        toggleLightModeButton.innerHTML ='Välj ljust läge'
-    console.og('ljust läge')
-    }
-    
+export function toggleLightMode() {
+  document.body.classList.toggle("darkmode");
+  if (document.body.classList.contains("darkmode")) {
+    toggleLightModeButton.innerHTML = "Välj mörkt läge";
+    console.log("ljust läge");
+  } else {
+    toggleLightModeButton.innerHTML = "Välj ljust läge";
+    console.log("mörkt läge");
+  }
 }
-
-export default toggleLightMode
+const toggleLightModeButton = document.querySelector(".toggle-btn");
+toggleLightModeButton.addEventListener("click", toggleLightMode);
+console.log(toggleLightModeButton);
