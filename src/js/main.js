e9d33e8602dc0f3f5d1231fe80fe678a997d7e80
petraelgemyr <petra.elgemyr@gmail.com>
2023-03-18 11:47:44 +0100

    (fix)removed toggle from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 import { createHtml } from "./createHtml";
-import { toggleLightMode } from "./toggleDarkmode";
 import "./../scss/main.scss";
 
 function init() {
-  // toggleLightMode();
   createHtml();
 }
 
